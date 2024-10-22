aa65e441574e00a262a644bfe1eb8e09b9d47859
Mahesh-Thorat-collab <msthorat2004@gmail.com>
2024-10-22 23:00:42 +0530

    Add files via upload

---
+++ SGC.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class SGC {
+   public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     System.out.println("***********STUDENTS GRADE CALCULATOR************");
+     System.out.println(" Enter  The Number Of Subjects");
+     //here s is the number of subjects.
+     int s=sc.nextInt();
+     int total=0;
+     System.out.println(" Enter the marks obtained in"+s+" subjects respectively:");
+       for(int i=0; i<s;i++); {
+        int marks=sc.nextInt();
+        total= total+marks;5
+     }
+     //AP is Average percentage
+      double AP=(double)total/s;
+      String Grade;
+      if(AP>=90){
+         Grade="+A";
+      }
+      else if (AP>=80) {
+        Grade="A";
+     }
+     else if (AP>=70) {
+        Grade="B";
+     }
+     else if (AP>=60) {
+        Grade="C";
+     }
+     else if (AP>=50) {
+        Grade="D";
+     }
+     else if (AP>=40) {
+        Grade="E";
+     }
+     else{
+        Grade="Fail";
+     }
+    System.out.println("total marks="+total);
+    System.out.println("Average Percentage="+AP+" %");
+    System.out.println(" Grade is:"+Grade);
+    }
+
+}
+
